4e827d1b0d66ffe53b9b4caee26909cebf18d44f
dejwona <iwograf@gmail.com>
2016-09-09 17:07:45 +0200

    sit on the chair

--- js/app.js
+++ js/app.js
@@ -8,8 +8,42 @@
     
     remove.addEventListener("mouseover", function(e) {
     	e.preventDefault();
-    	apla.parentNode.removeChild(apla);
-    	line.parentNode.removeChild(line);
+        if(this.children.length>1) {
+        apla.parentNode.removeChild(apla);
+        line.parentNode.removeChild(line);   
+        } 
+    	return null;
     });
 
+    // zad 3
+    var arrowLeft = document.querySelector(".arrowLeft");
+    var arrowRight = document.querySelector(".arrowRight");
+    var bigpicture = document.querySelector(".bigpicture img");
+    var images = ["./images/black_chair.png", "./images/orange.png", "./images/red.png"];
+
+    var imageNumber = 0;
+    var imageLength = images.length - 1;
+
+    arrowRight.addEventListener("click", function(e) {        
+        imageNumber++;
+            if (imageNumber > imageLength) {
+                imageNumber = 0;
+            } 
+            bigpicture.src = images[imageNumber];
+    });
+
+    arrowLeft.addEventListener("click", function(e) {       
+        imageNumber--;
+            if (imageNumber < 0) {
+                imageNumber = imageLength;
+            }
+            bigpicture.src = images[imageNumber];
+    });
+
+
+    // zad *
+
+
+
 });
+ 
