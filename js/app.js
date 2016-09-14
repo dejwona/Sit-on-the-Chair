8fcd700f1b65e65cf74f080d111e365e4c1325cc
dejwona <iwograf@gmail.com>
2016-09-14 19:40:06 +0200

    sit

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@
     	return null;
     });
 
+
     // zad 3
     var arrowLeft = document.querySelector(".arrowLeft");
     var arrowRight = document.querySelector(".arrowRight");
@@ -43,26 +44,52 @@
 
     // zad *
     // a)
-    var selectOption = document.querySelectorAll(".list_panel");
+    var selectOption = document.querySelectorAll(".list_panel li");
     var list_arrow = document.querySelectorAll(".list_arrow");
     var suma = document.querySelector(".sum");
 
+    var title = document.querySelector(".title");
+    var color = document.querySelector(".color");
+    var pattern = document.querySelector(".pattern");
+    var transport = document.querySelector(".transport");
 
-    for (var i =0, len = list_arrow.length; i < len; i++) {
+
+    for (var i = 0, len = list_arrow.length; i < len; i++) {
+
         list_arrow[i].addEventListener("click", function(e) {
             console.log("dziala2");
             var sibling = this.nextElementSibling;
             if (sibling !== null) {
                 sibling.style.display = "block";
-                if (selectOption[0]) {
-                    console.log("dziala3");
-                    suma.innerText = "200";
-                } else if (selectOption[1]) {
-                    suma.innerText = "300";
+
+               
+                for (var j = 0, len = selectOption.length; j < len; j++) {
+                    selectOption[j].addEventListener("click", function(e) {
+                        
+                    if (selectOption[0]) {
+                        console.log("dziala3");
+                        suma.innerText = "200";
+                        title.innerText = "Clair";
+                    } else if (selectOption[1]) {
+                        console.log("dziala4");
+                        suma.innerText = "300";
+                        title.innerText = "Margarita";
+                    } else if (selectOption[2]) {
+                        console.log("dziala5");
+                        suma.innerText = "400";
+                        title.innerText = "Selena";
+                    }
+                });
+                
+
                 }
-            }
+            } 
+            
         });
+
     };
+
+
 
 
     // b) dziala
