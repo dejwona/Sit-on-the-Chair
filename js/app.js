9fb70eec588645687217fe73fe699a94a4c83617
dejwona <iwograf@gmail.com>
2016-10-01 21:10:24 +0200

    sit

--- js/app.js
+++ js/app.js
@@ -44,14 +44,95 @@
 
     // zad *
     // a)
+    var selects = document.querySelectorAll(".list_panel");
     var selectOption = document.querySelectorAll(".list_panel li");
     var list_arrow = document.querySelectorAll(".list_arrow");
     var suma = document.querySelector(".sum");
 
     var title = document.querySelector(".title");
-    var color = document.querySelector(".color");
-    var pattern = document.querySelector(".pattern");
     var transport = document.querySelector(".transport");
+
+    var sort = document.querySelectorAll("#sort li");
+    var color = document.querySelectorAll("#color li");
+    var material = document.querySelectorAll("#material li");
+
+    var sortResult = document.querySelectorAll("#sort li");
+    var colorResult = document.querySelectorAll("#color li");
+    var materialResult = document.querySelectorAll("#material li");
+
+
+
+    var transportPrice = document.getElementById('transport').dataset.transportPrice;
+    var transportChecked = document.getElementById('transport').checked;
+    var optionsSortPrice = 0;
+    var optionsSortName = '';
+    var optionsColorPrice = 0;
+    var optionsColorName = '';
+    var optionsMaterialPrice = 0;
+    var optionsMaterialName = '';
+    //options.material.price = 0;
+    //options.transport.price = 0;
+    
+    for (var i = 0; i < sort.length; i ++) {
+        sort[i].addEventListener("click", function(e) {
+            optionsSortPrice = this.dataset.sortPrice;
+            console.log(this);
+            optionsSortName = this.dataset.sortName;
+            show();
+        });
+    }
+
+    for (var i = 0; i < color.length; i ++) {
+        color[i].addEventListener("click", function(e) {
+            optionsColorPrice = this.dataset.colorPrice;
+            console.log(this);
+            optionsColorName = this.dataset.colorName;
+            show();
+        });
+    }
+
+    for (var i = 0; i < material.length; i ++) {
+        material[i].addEventListener("click", function(e) {
+            optionsMaterialPrice = this.dataset.materialPrice;
+            console.log(this);
+            optionsMaterialName = this.dataset.materialName;
+            show();
+        });
+    }
+
+    document.getElementById('transport').addEventListener("click", function(e) {
+        console.log('transport');
+        show();
+    });
+
+    var show = function() {
+        
+        var titleName = document.getElementById('titleName');
+        var colorName = document.getElementById('colorName');
+        var patternName = document.getElementById('patternName');
+        var transportName = document.getElementById('transportName');
+
+        var titleValue = document.getElementById('titleValue');
+        var colorValue = document.getElementById('colorValue');
+        var patternValue = document.getElementById('patternValue');
+        var transportValue = document.getElementById('transportValue');
+
+        titleName.innerText = optionsSortName;
+        titleValue.innerText = optionsSortPrice;
+        colorName.innerText = optionsColorName;
+        colorValue.innerText = optionsColorPrice;
+        patternName.innerText = optionsMaterialName;
+        patternValue.innerText = optionsMaterialPrice;
+        console.log(document.getElementById('transport').checked);
+        if (transportChecked==true){
+            transportValue.innerText = transportPrice;
+        } else {
+           transportValue.innerText = 0; 
+        }
+        
+    }
+
+    
 
 
     for (var i = 0, len = list_arrow.length; i < len; i++) {
@@ -59,10 +140,29 @@
         list_arrow[i].addEventListener("click", function(e) {
             console.log("dziala2");
             var sibling = this.nextElementSibling;
-            if (sibling !== null) {
-                sibling.style.display = "block";
 
-               
+            
+            
+            if (sibling !== null) {
+
+                for (var j = 0; j < selects.length; j++) {
+                    console.log("block");
+
+                    if(selects[j]==sibling){
+
+                        if (sibling.style.display === "block") {
+                            sibling.style.display = "none";
+                        } else {
+                            sibling.style.display = "block";
+                        }
+                    } else {
+                        selects[j].style.display = "none";
+                    }
+                }
+                
+                
+
+               /*
                 for (var j = 0, len = selectOption.length; j < len; j++) {
                     selectOption[j].addEventListener("click", function(e) {
                         
@@ -81,8 +181,10 @@
                     }
                 });
                 
+                
 
                 }
+                */
             } 
             
         });
